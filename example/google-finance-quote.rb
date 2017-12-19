aa2fd254831b06d027a846ab2171f0a5a294bba0
Kouhei Sutou <kou@clear-code.com>
2017-12-19 20:32:40 +0900

    Use more suitable name

--- example/google-finance-quote.rb
+++ example/google-finance-quote.rb
@@ -4,7 +4,7 @@ require "data-reader"
 
 reader = DataReader::GoogleFinanceQuoteReader.new(["AMZN", "GOOG"])
 reader.each do |entry|
-  p [entry.name, entry.change_percent, entry.last, entry.time]
+  p [entry.ticker_symbol, entry.change_percent, entry.last, entry.time]
   # ["AMZN", 0.09, 1039.87, Time.parse("2017-07-25T16:00:00Z")]
   # ["GOOG", -3.02, 950.70, Time.parse("2017-07-25T16:00:00Z")]
 end
